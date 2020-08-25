5d938a8abd5b78786c78bd71aecec539ca7b42aa
Rustin-Liu <rustin.liu@gmail.com>
2020-08-25 11:21:59 +0800

    feat: add ChallengePulls

---
+++ src/db/entity/ChallengePulls.ts
@@ -0,0 +1,19 @@
+import {Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn} from "typeorm";
+
+@Entity({name: "challenge_pulls"})
+export class ChallengePulls {
+    @PrimaryColumn({name: "pull_id", nullable: false})
+    pullId: number;
+
+    @Column({name: "reward", nullable: false, default: 0})
+    reward: number;
+
+    @Column({name: "challenge_issue_id", nullable: false})
+    challengeIssueId: number;
+
+    @CreateDateColumn({name: "created_at"})
+    createdAt: Date;
+
+    @UpdateDateColumn({name: "update_at"})
+    updatedAt: Date;
+}
---
+++ src/db/migration/1598325417454-ChallengePulls.ts
@@ -0,0 +1,62 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class ChallengePulls1598325417454 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: "challenge_pulls",
+            columns: [
+                {
+                    name: "pull_id",
+                    type: "int(11)",
+                    isPrimary: true,
+                    isNullable: false,
+                },
+                {
+                    name: "reward",
+                    type: "int(11)",
+                    isNullable: false,
+                    default: 0,
+                },
+                {
+                    name: "challenge_issue_id",
+                    type: "int(11)",
+                    isNullable: false,
+                },
+                {
+                    name: "created_at",
+                    type: "timestamp",
+                    isNullable: false,
+                    default: 'now()',
+                },
+                {
+                    name: "updated_at",
+                    type: "timestamp",
+                    isNullable: false,
+                    default: 'now()',
+                }
+            ],
+            foreignKeys:[
+                {
+                    columnNames:["pull_id"],
+                    referencedTableName: "pulls",
+                    referencedColumnNames:["id"],
+                    onDelete: "CASCADE",
+                    onUpdate: "CASCADE"
+                },
+                {
+                    columnNames:["challenge_issue_id"],
+                    referencedTableName: "challenge_issues",
+                    referencedColumnNames:["issue_id"],
+                    onDelete: "CASCADE",
+                    onUpdate: "CASCADE"
+                }
+            ]
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("challenge_pulls)")
+    }
+
+}
