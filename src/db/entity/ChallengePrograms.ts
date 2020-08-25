827a5dd7198093618c91020b878bd29d1e6894e4
Rustin-Liu <rustin.liu@gmail.com>
2020-08-25 10:47:57 +0800

    feat: add ChallengePrograms

---
+++ src/db/entity/ChallengePrograms.ts
@@ -0,0 +1,31 @@
+// @ts-ignore
+import {Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn} from "typeorm";
+
+
+@Entity({name: "challenge_programs"})
+export class ChallengePrograms {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({name: "program_theme", unique:true, nullable: false})
+    programTheme: string;
+
+    @Column({name: "program_name", nullable: true, default: null})
+    programName: string;
+
+    @Column({name: "program_desc", nullable: true, default: null})
+    programDesc: string;
+
+    @Column({name: "begin_at", type: "timestamp", nullable: true, default: null})
+    beginAt: Date;
+
+    @Column({name: "end_at", type: "timestamp", nullable: true, default: null})
+    endAt: Date;
+
+    @CreateDateColumn({name: "created_at"})
+    createdAt: Date;
+
+    @UpdateDateColumn({name: "update_at"})
+    updatedAt: Date;
+
+}
---
+++ src/db/migration/1598320483479-ChallengePrograms.ts
@@ -0,0 +1,64 @@
+// @ts-ignore
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class ChallengePrograms1598320483479 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: "challenge_programs",
+            columns: [
+                {
+                    name: "id",
+                    type: "int(11)",
+                    isGenerated: true,
+                    isPrimary: true
+                },
+                {
+                    name: "program_theme",
+                    type: "varchar(255)",
+                    isUnique: true,
+                    isNullable: false
+                }, {
+                    name: "program_name",
+                    type: "text",
+                    isNullable: true,
+                    default: null,
+                },
+                {
+                    name: "program_desc",
+                    type: "text",
+                    isNullable: true,
+                    default: null,
+                },
+                {
+                    name: "begin_at",
+                    type: "timestamp",
+                    isNullable: true,
+                    default: null,
+                },
+                {
+                    name: "end_at",
+                    type: "timestamp",
+                    isNullable: true,
+                    default: null,
+                },
+                {
+                    name: "created_at",
+                    type: "timestamp",
+                    isNullable: false,
+                    default: 'now()',
+                },
+                {
+                    name: "updated_at",
+                    type: "timestamp",
+                    isNullable: false,
+                    default: 'now()',
+                }
+            ]
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("challenge_programs)")
+    }
+}
