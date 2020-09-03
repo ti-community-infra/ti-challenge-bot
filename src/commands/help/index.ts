61e3d50a595ac0904ec1af1590d2b9370e2ce2eb
二手掉包工程师 <rustin.liu@gmail.com>
2020-09-03 14:51:13 +0800

    refactor: improve help message (#53)

--- src/commands/help/index.ts
+++ src/commands/help/index.ts
@@ -14,11 +14,15 @@ The commands are:
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;**/reward score**&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;reward the score to PR
     
 ----------------------------------------------------------------------------------------------------------
+<details>
+<summary>More Details</summary>
+
 >You can find more details on [challenge-bot-docs](https://tidb-community-bots.github.io/challenge-bot/).
 >
 >If you think there is a problem, you can report bug on [report-a-bug](https://github.com/tidb-community-bots/challenge-bot/issues/new/choose).
 >
 >If you think the bot is dead, please contact [Rustin-Liu](https://github.com/Rustin-Liu) to fix it.
+</details>
 `
 const help = async (context: Context) => {
   await context.github.issues.createComment(context.issue({ body: helpMessage }))
