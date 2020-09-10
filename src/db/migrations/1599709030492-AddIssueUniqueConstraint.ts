3803452e06975f4e86e9d0ac00b93905b1092322
Rustin-Liu <rustin.liu@gmail.com>
2020-09-10 12:03:56 +0800

    fix: refine lint

--- src/db/migrations/1599709030492-AddIssueUniqueConstraint.ts
+++ src/db/migrations/1599709030492-AddIssueUniqueConstraint.ts
@@ -1,13 +1,12 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+// eslint-disable-next-line no-unused-vars
+import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class AddIssueUniqueConstraint1599709030492 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('alter table issues add constraint owner_repo_issue_number unique(owner,repo,issue_number)')
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query('alter table issues add constraint owner_repo_issue_number unique(owner,repo,issue_number)')
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query('alter table issues drop index owner_repo_issue_number;')
-    }
-
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('alter table issues drop index owner_repo_issue_number;')
+  }
 }
