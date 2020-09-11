7ee27322c4c61f2c4e9db41976e6dd3ee70214c3
Rustin-Liu <rustin.liu@gmail.com>
2020-09-11 15:13:02 +0800

    fix: refine lint

--- src/api/challenge-team/index.ts
+++ src/api/challenge-team/index.ts
@@ -4,7 +4,8 @@ import ChallengeTeamService from '../../services/challenge-team'
 import { Request, Response } from 'express'
 // eslint-disable-next-line no-unused-vars
 import { ChallengeTeamQuery } from '../../commands/queries/ChallengeTeamQuery'
-const createTeam = async (req:Request, res:Response, challengeTeamService: ChallengeTeamService) => {
+
+const createTeam = async (req: Request, res: Response, challengeTeamService: ChallengeTeamService) => {
   const challengeTeamQuery = <ChallengeTeamQuery>req.body
 
   const response = await challengeTeamService.create(challengeTeamQuery)
