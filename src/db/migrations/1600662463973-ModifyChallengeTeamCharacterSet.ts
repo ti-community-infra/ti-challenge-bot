e74f1861d4a89efb27519a95651beea87580dfea
Rustin-Liu <rustin.liu@gmail.com>
2020-09-21 12:43:07 +0800

    fix: refine db character set issue

---
+++ src/db/migrations/1600662463973-ModifyChallengeTeamCharacterSet.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class ModifyChallengeTeamCharacterSet1600662463973 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('alter table challenge_teams convert to character set utf8mb4 collate utf8mb4_bin;')
+    await queryRunner.query('alter table challenge_teams default character set utf8mb4 collate utf8mb4_bin;')
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('alter table challenge_teams convert to character set latin1 collate latin1_swedish_ci;')
+    await queryRunner.query('alter table challenge_teams default character set latin1 collate latin1_swedish_ci;')
+  }
+}
